22cbfaf335f847136ba5632294a073bcbc73770a
Joshua Taggart <joshua.taggart@usu.edu>
2026-02-28 18:22:40 -0700

    update teleop launch

--- stiv_bringup/launch/teleop_launch.py
+++ stiv_bringup/launch/teleop_launch.py
@@ -1,5 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
@@ -29,9 +31,21 @@ def generate_launch_description():
         ],
     )
 
+    ns_arg = DeclareLaunchArgument("namespace", default_value="", description="Namespace for the node")
+
+    # Driver node for the rosmaster
+    driver_node = Node(
+        package="rosmaster_r2_akm_driver",
+        executable="ackman_driver_r2",
+        parameters=[{"driver_sleep_time": 0.002, "state_pub_period": 1.0}],
+        namespace=LaunchConfiguration("namespace"),
+    )
+
     return LaunchDescription(
         [
             joy_node,
             teleop_node,
+            ns_arg,
+            driver_node,
         ]
     )
